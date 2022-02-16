b09b02dabf2aa1bf1a063600ca53fa969a747a85
Gabriel Gomes de Almeida <gabrielda@.ciandt.com>
2022-02-16 12:47:08 -0300

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -23,15 +23,36 @@ const App = () => {
         setNameInput(e.target.value);
     }
 
+    const deletePerson = (id: string) => {
+        dispatch({
+            type: 'DEL',
+            payload: { id}
+        });
+    }
+
+    const handleOrderButton = () => {
+        dispatch({
+            type: 'ORDER'
+        })
+    }
+
     return (
         <div className="p-5">
             <input className="border-2" type="text" value={nameInput} onChange={handleInputChange}/>
             <button onClick={handleAddButton}>Adicionar</button>
 
+            <hr />
+            
+            <button onClick={handleOrderButton}>Ordenar</button>
+            <br />
+
             Lista de pessoas:
             <ul>
                 {list.map((item, index) => (
-                    <li key={index}>{item.name}</li>
+                    <li key={index}>
+                        {item.name}
+                        <button className='ml-2' onClick={() => deletePerson(item.id)}> Deletar </button>    
+                    </li>
                 ))}
             </ul>
         </div>
